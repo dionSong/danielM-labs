df38b4effdd5f81593b11f982185e6a91d030ea2
dionSong <martinezdaniel082014@gmail.com>
2017-09-22 19:47:24 -0500

    casi terminada la pagina

--- lab-2-scaffolding/script.js
+++ lab-2-scaffolding/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     $('.carousel.carousel-slider').carousel({fullWidth: true});
     $('.parallax').parallax();    
+    $(".button-collapse").sideNav();
+    $('.carousel.carousel-slider').on('click', function(){
+        $('.carousel.carousel-slider').carousel('next');
+    });
 });

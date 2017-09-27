e44e38c69b6b5e34c00b70875f739f39737e0bd4
dionSong <martinezdaniel082014@gmail.com>
2017-09-26 23:14:35 -0500

    added last loop on second lab js

--- 99-lines-of-code/scripts.js
+++ 99-lines-of-code/scripts.js
@@ -1,10 +1,13 @@
 
-var friends = ['Ana:', 'Agustin:', 'Facundo:', 'Eneko:', 'Juan:'];
+var friends = ['Ana', 'Agustin', 'Facundo', 'Eneko', 'Juan'];
 
 for (a = 0; a <= 4; a++){
-    console.log(friends[a]);
+    console.log(friends[a] + ':');
     for (i = 99; i > 0; i--){
-        console.log(i + 'lines of code in the file, ' + i + 'lines of code; ' + friends[a] + 'strikes one out, clears it all out,' + (i - 1 ) + 'lines of code in the file');
+         console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friends[a] + ' strikes one out, clears it all out, ' + (i - 1 ) + ' lines of code in the file');
+    }
+    if (i === 1){
+        console.log(i + ' line of code in the file, ' + i + ' line of code; ' + friends[a] + ' strikes one out, clears it all out, no more lines of code in the file');
     }
 }      
 

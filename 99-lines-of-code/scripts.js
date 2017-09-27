e8d8c3f059b1f9e6d6ea73dc718f33b9d14a1cf2
dionSong <martinezdaniel082014@gmail.com>
2017-09-26 19:25:48 -0500

    finished lab 1 js and started second one

---
+++ 99-lines-of-code/scripts.js
@@ -0,0 +1,10 @@
+
+var friends = ['Ana:', 'Agustin:', 'Facundo:', 'Eneko:', 'Juan:'];
+
+for (a = 0; a <= 4; a++){
+    console.log(friends[a]);
+    for (i = 99; i > 0; i--){
+        console.log(i + 'lines of code in the file, ' + i + 'lines of code; ' + friends[a] + 'strikes one out, clears it all out,' + (i - 1 ) + 'lines of code in the file');
+    }
+}      
+

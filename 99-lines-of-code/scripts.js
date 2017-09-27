939dc230d87602b2b4317e9f2f6778293572d592
dionSong <martinezdaniel082014@gmail.com>
2017-09-27 18:34:42 -0500

    started practicing css grid

--- 99-lines-of-code/scripts.js
+++ 99-lines-of-code/scripts.js
@@ -3,7 +3,7 @@ var friends = ['Ana', 'Agustin', 'Facundo', 'Eneko', 'Juan'];
 
 for (a = 0; a <= 4; a++){
     console.log(friends[a] + ':');
-    for (i = 99; i > 0; i--){
+    for (i = 99; i > 1; i--){
          console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friends[a] + ' strikes one out, clears it all out, ' + (i - 1 ) + ' lines of code in the file');
     }
     if (i === 1){

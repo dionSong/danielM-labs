8ff73237bcf34db4d534e24687d6ff59fdda2c29
dionSong <martinezdaniel082014@gmail.com>
2017-10-01 14:19:28 -0500

    did square labs again

--- squares-lab/scripts.js
+++ squares-lab/scripts.js
@@ -1,83 +1,80 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+//Add Square Btn
+var squareBtn = document.createElement('button');
+var spanBtn = document.createElement('span');
+squareBtn.className = 'btn';
+spanBtn.innerText = 'Add a Square!';
 
 
-//Button
-var addSquareBtn = document.createElement('button');
-var innerSpan = document.createElement('span');
-innerSpan.innerText = 'Add Square!';
+squareBtn.appendChild(spanBtn);
+document.body.appendChild(squareBtn);
 
-addSquareBtn.appendChild(innerSpan);
-//addSquareBtn.innerText = 'Add Square';
-addSquareBtn.className = 'btn';
-document.body.appendChild(addSquareBtn);
+//Btn On Click Create a Div
+squareBtn.addEventListener('click', createDiv);
 
+    function createDiv(){
+        var newDiv = document.createElement('div');
+        newDiv.className = 'square';
 
-//Click Btn Function
+        //Push to the array before .length
+        totalSquares.push(newDiv);
+        newDiv.id = totalSquares.length;
 
-addSquareBtn.addEventListener('click', createSquare);
+        document.body.appendChild(newDiv);
 
-function createSquare(){
-    var newDiv = document.createElement('div');
-    newDiv.className = 'square';
-
-    
-    newDiv.setAttribute('id', totalSquares.length + 1);
-
-    document.body.appendChild(newDiv);
-    totalSquares.push(newDiv); 
-
-    //Hover Function
-        newDiv.addEventListener('mouseover', mouseOverFunction);
-        function mouseOverFunction(){
+        //On Hover show ID
+        newDiv.addEventListener('mouseover', showId);
+        newDiv.addEventListener('mouseout', hideId);
+        
+        function showId(){
             newDiv.innerText = newDiv.id;
-            
         }
-    //Unhover Function   
-        newDiv.addEventListener('mouseout', mouseOutFunction);
-        function mouseOutFunction(){
+        
+        function hideId(){
             newDiv.innerText = ' ';
         }
 
-    // One Click div function
-        newDiv.addEventListener('click', function(){
-            newDiv.style.backgroundColor = getRandomColor();
-        });
+        //Div On Click change Color
+        newDiv.addEventListener('click', changeColor);
 
-    //Double Click div function
-        newDiv.addEventListener('dblclick', function(){
+        function changeColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+                newDiv.style.backgroundColor = color;
+            }
+        }
 
-        if (newDiv.previousElementSibling === addSquareBtn){
-            alert('there isn\'t a square before the clicked square ');
-        }
-         else if (newDiv.nextElementSibling === null && newDiv.id % 2 === 0){
-            alert('there isn\'t a square after the clicked square ');
-        }
-        
-        //even
-        else if (newDiv.id % 2 === 0){
-        newDiv.nextSibling.remove();
-        }
-        //odd
-        else if (newDiv.id % 2) {
-        newDiv.previousSibling.remove();
-        }   
-        
-            
-        })
-}
-// array.splice(index, 1);
+        //Div on Double Click Remove Squares
+        newDiv.addEventListener('dblclick', removeSquare);
+
+        function removeSquare(){
+            if (newDiv.id % 2 === 0 && newDiv.nextSibling != null)
+                newDiv.nextElementSibling.remove();
+            else if (newDiv.id % 2 === 1 && newDiv.previousSibling != squareBtn)
+                newDiv.previousElementSibling.remove();
+            else if (newDiv.nextSibling === null)
+                alert('there isn\'t a square after');
+            else if (newDiv.previousSibling === squareBtn)
+                alert('there isn\'t a square before');
+        }    
+    }
+
 });
 
-//Get Random Color Function
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  } 
-
-//Total Squares Array
 var totalSquares = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
